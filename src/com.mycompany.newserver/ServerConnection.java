8be6ae0ff53806fb7596377eb0f0778cfb3e9de6
SaraAljuraybah <Saraaljrebah1@gmail.com>
2025-10-17 21:42:01 +0300

    1

---
+++ src/com.mycompany.newserver/ServerConnection.java
@@ -0,0 +1,55 @@
+
+package com.mycompany.newserver;
+
+/**
+ *
+ * @author Sss43
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author User
+ */
+public class ServerConnection implements Runnable{
+    private Socket server;
+    private BufferedReader in;
+    private PrintWriter out;
+      
+    public ServerConnection (Socket s) throws IOException{
+        server=s;
+        in= 
+          new BufferedReader (new InputStreamReader(server.getInputStream())); 
+    out=new PrintWriter(server.getOutputStream(),true); 
+    }
+
+    @Override
+    public void run(){
+        
+            String serverResponse;
+            try {
+                while(true){
+                serverResponse = in.readLine(); 
+                if (serverResponse==null) break;
+                System.out.println("Server says: "+serverResponse);
+                }
+                
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            } finally{
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }     
+    }           
+}
+
