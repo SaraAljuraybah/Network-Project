98789809067d05ded92782e3a17aecbc322f207e
Dana <danaffo0s@gmail.com>
2025-10-18 23:01:52 +0300

    Add files via upload
    uploaded by dana :)

--- src/com.mycompany.newserver/NewServer.java
+++ src/com.mycompany.newserver/NewServer.java
@@ -1,23 +1,31 @@
-
-package com.mycompany.lab3;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.phase1;
 
 /**
  *
- * @author Sss43
+ * @author HUAWEI
  */
 import java.io.*; 
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.ArrayList;
-
-public class NewServer
-{
+public class NewServer {
+    //array for reservations: 
+   private static ArrayList <Reservation> Reservations= new ArrayList<>();
+    //can we use an array insted of a list? or we dont know the size in advance?
     
-     // array for reservation 
-    private static ArrayList <Reservation> Reservations = new ArrayList<>();////
     private static ArrayList<NewClient> clients=new ArrayList<>();
     
-    
+    //////
+    private static Resturant[] allRresturants={
+      new Resturant("R1"),
+      new Resturant("R2"),
+      new Resturant("R3"),
+
+    };// method
     public static void main(String[] args) throws IOException
     {
         ServerSocket serverSocket = new ServerSocket(9090);
@@ -26,11 +34,10 @@ public class NewServer
          System.out.println("Waiting for client connection");
          Socket client=serverSocket.accept();
          System.out.println("Connected to client");
-         NewClient clientThread=new NewClient(client,clients, Reservations);//// // new thread 
+         NewClient clientThread=new NewClient(client,clients, Reservations); // new thread 
          clients.add(clientThread);
          new Thread (clientThread).start();
          
         }
     }
 }
-
