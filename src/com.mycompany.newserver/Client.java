5755640c2184a10ddbce6c7adc279a5c0419f03e
SaraAljuraybah <Saraaljrebah1@gmail.com>
2025-10-18 13:20:16 +0300

    Add files via upload
    Gharsah, Haya, Sara work

---
+++ src/com.mycompany.newserver/Client.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ *
+*/
+
+package com.mycompany.lab3;
+/**
+ *
+ * @author Sss43
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    private static final String Server_IP="localhost";
+    private static final int Server_port=9090;
+    private  BufferedReader in;
+    private PrintWriter out;
+    private Socket socket;
+            
+    public Client(){
+    
+    try {
+            socket = new Socket(Server_IP, Server_port);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("Connected to server.");
+        } catch (IOException e) {
+            System.out.println("Failed to connect: " + e.getMessage());
+        }
+    
+ 
+    }
+    
+  public void register(String username, String password) {
+        if (out != null) {
+            //out.println("Regestration info " + username + " " + password);
+            out.println( username );
+            out.println( password );       
+            System.out.println("Sent to server: Regestration info " + username + " " + password);
+        }
+    }
+        /**public static void main(String[] args) throws IOException{
+          try(Socket socket = new Socket (Server_IP,Server_port)) {
+              ServerConnection servcon=new ServerConnection(socket);
+              BufferedReader keyboard=
+                      new BufferedReader (new InputStreamReader(System.in));
+              PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
+              new Thread (servcon).start(); 
+              try{
+                  while(true){
+                      MainMenu main= new MainMenu();
+                      main.setVisible(true);
+                      
+                      
+                      String command=keyboard.readLine();
+                      out.println(command);
+                      
+                  } // end of while loop
+              } catch (Exception e){
+                  e.printStackTrace();
+              }
+          }
+              System.exit(0); }**/
+        
+
+
+
+
+}
--- src/com.mycompany.newserver/NewServer.java
+++ src/com.mycompany.newserver/NewServer.java
@@ -1,31 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.phase1;
+
+package com.mycompany.lab3;
 
 /**
  *
- * @author HUAWEI
+ * @author Sss43
  */
 import java.io.*; 
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.util.ArrayList;
-public class NewServer {
-    //array for reservations: 
-    ArrayList <Reservation> Reservations= new ArrayList<>();
-    //can we use an array insted of a list? or we dont know the size in advance?
+
+public class NewServer
+{
     
+     // array for reservation 
+    private static ArrayList <Reservation> Reservations = new ArrayList<>();////
     private static ArrayList<NewClient> clients=new ArrayList<>();
     
-    //////
-    private static Resturant[] allRresturants={
-      new Resturant("R1"),
-      new Resturant("R2"),
-      new Resturant("R3"),
-
-    };// method
+    
     public static void main(String[] args) throws IOException
     {
         ServerSocket serverSocket = new ServerSocket(9090);
@@ -34,10 +26,11 @@ public class NewServer {
          System.out.println("Waiting for client connection");
          Socket client=serverSocket.accept();
          System.out.println("Connected to client");
-         NewClient clientThread=new NewClient(client,clients); // new thread 
+         NewClient clientThread=new NewClient(client,clients, Reservations);//// // new thread 
          clients.add(clientThread);
          new Thread (clientThread).start();
          
         }
     }
 }
+
